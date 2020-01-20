a6d64703843266f68d6b048695c3caaa7ca064f1
brandon <brandonpark206@gmail.com>
2020-01-19 21:52:24 -0800

    create bookable and booking models, migrations. created an availability component to bookable component

---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h6 class="text-uppercase text-secondary font-weight-bolder">Check Availability</h6>
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="from">From</label>
+                <input 
+                    type="text"
+                    name="from"
+                    class="form-control form-control-sm"
+                    placeholder="Start date"
+                    v-model="from"
+                    @keyup.enter="check"
+                >
+            </div>
+            <div class="form-group col-md-6">
+                <label for="to">To</label>
+                <input 
+                    type="text"
+                    name="to"
+                    class="form-control form-control-sm"
+                    placeholder="End date"
+                    v-model="to"
+                    @keyup.enter="check"
+                >
+            </div>
+        </div>
+
+        <button class="btn btn-secondary btn-block" @click="check">Check!</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            from: null,
+            to: null,
+        };
+    },
+    methods: {
+        check() {
+            alert("I'm checking right now!")
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    label {
+        font-size: 0.7rem;
+        text-transform: uppercase;
+        color: gray;
+        font-weight: bolder;
+    }
+</style>
