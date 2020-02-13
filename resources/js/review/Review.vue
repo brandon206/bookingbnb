1d3790018849e8ef5961b16a4718e4720e8aa3fe
brandon <brandonpark206@gmail.com>
2020-02-12 23:40:06 -0800

    handling fatal errors and using shared javascript across components

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,111 +1,150 @@
 <template>
   <div>
-      <div class="row" v-if="error">Unknown error has occured, please try again later!</div>
-      <div class="row" v-else>
-        <div :class="[{'col-md-4' : twoColumns}, {'d-none': oneColumn}]">
-            <div class="card">
-                <div class="card-body">
-                    <div v-if="loading">Loading...</div>
-                    <div v-if="hasBooking">
-                        <p>
-                            Stayed at <router-link :to="{name: 'bookable', params: { id: booking.bookable.bookable_id }}">
-                            {{ booking.bookable.title }}
-                            </router-link>
-                        </p>
-                        <p>From {{ booking.from }} to {{ booking.to }}</p>
-                    </div>
-                </div>
+    <fatal-error v-if="error"></fatal-error>
+    <div class="row" v-else>
+      <div :class="[{'col-md-4': twoColumns}, {'d-none': oneColumn}]">
+        <div class="card">
+          <div class="card-body">
+            <div v-if="loading">Loading...</div>
+            <div v-if="hasBooking">
+              <p>
+                Stayed at
+                <router-link
+                  :to="{name: 'bookable', params: { id: booking.bookable.bookable_id}}"
+                >{{ booking.bookable.title }}</router-link>
+              </p>
+              <p>From {{ booking.from }} to {{ booking.to }}</p>
             </div>
-        </div>
-        <div :class="[{'col-md-8' : twoColumns}, {'col-md-12' : oneColumn}]">
-            <div v-if="this.loading">Loading...</div>
-            <div v-else>
-                <div v-if="this.alreadyReviewed">
-                    <h3> You've already left a review for this booking</h3>
-                </div>
-                <div v-else>
-                    <div class="form-group">
-                        <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-                        <StarRating 
-                            class="fa-3x"
-                            v-model="review.rating"
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="content" class="text-muted">Describe your experience with</label>
-                        <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
-                    </div>
-
-                    <button class="btn btn-lg btn-primary btn-block">Submit</button>
-                </div>
-            </div>
+          </div>
         </div>
       </div>
+      <div :class="[{'col-md-8': twoColumns}, {'col-md-12': oneColumn}]">
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+          <div v-if="alreadyReviewed">
+            <h3>You've already left a review for this booking!</h3>
+          </div>
+          <div v-else>
+            <div class="form-group">
+              <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+              <star-rating class="fa-3x" v-model="review.rating"></star-rating>
+            </div>
+            <div class="form-group">
+              <label for="content" class="text-muted">Describe your experience with</label>
+              <textarea
+                name="content"
+                cols="30"
+                rows="10"
+                class="form-control"
+                v-model="review.content"
+                :class="[{'is-invalid': errorFor('content')}]"
+              ></textarea>
+              <div
+                class="invalid-feedback"
+                v-for="(error, index) in errorFor('content')"
+                :key="'content' + index"
+              >{{ error }}</div>
+            </div>
+
+            <button
+              class="btn btn-lg btn-primary btn-block"
+              @click.prevent="submit"
+              :disabled="sending"
+            >Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { is404 } from "./../shared/utils/response";
-
+import { is404, is422 } from "./../shared/utils/response";
 export default {
-    data() {
-        return {
-            review: {
-                rating: 5,
-                content: null,
-            },
-            loading: false,
-            existingReview: null,
-            booking: null,
-            error: false,
-        };
-    },
-    created() {
-        this.loading = true;
-        //1. if review already exists (in reviews table by id)
-        axios
-            .get(`/api/reviews/${this.$route.params.id}`)
+  data() {
+    return {
+      review: {
+        id: null,
+        rating: 5,
+        content: null
+      },
+      existingReview: null,
+      loading: false,
+      booking: null,
+      error: false,
+      errors: null,
+      sending: false,
+    };
+  },
+  created() {
+    this.review.id = this.$route.params.id;
+    this.loading = true;
+    // 1. If review already exists (in reviews table by id)
+    axios
+      .get(`/api/reviews/${this.review.id}`)
+      .then(response => {
+        this.existingReview = response.data.data;
+      })
+      .catch(err => {
+        if (is404(err)) {
+          // 2. Fetch a booking by a review key
+          return axios
+            .get(`/api/booking-by-review/${this.review.id}`)
             .then(response => {
-                this.existingReview = response.data.data;
+              this.booking = response.data.data;
             })
-            //only goes in here if review doesn't already exist
             .catch(err => {
-                if(is404(err)) {
-                    //2. fetch a booking by a review key
-                    return axios.get(`/api/booking-by-review/${this.$route.params.id}`).then(response => {
-                        this.booking = response.data.data;
-                    }).catch((err) => {
-                        if(! is404(err)){
-                            this.error = true;
-                        }
-                    });
-                }
-                this.error = true;
-            }).then(() => {
-                this.loading = false;
+              this.error = !is404(err);
             });
-        //3. store the review
+        }
+        this.error = true;
+      })
+      .then(() => {
+        this.loading = false;
+      });
+  },
+  computed: {
+    alreadyReviewed() {
+      return this.hasReview || !this.booking;
     },
-    computed: {
-        alreadyReviewed() {
-            return this.hasReview || ! this.booking;
-        },
-        hasReview() {
-            return this.existingReview !== null;
-        },
-        hasBooking() {
-            return this.booking !== null;
-        },
-        oneColumn() {
-            return ! this.loading && this.alreadyReviewed;
-        },
-        twoColumns() {
-            return this.loading || ! this.alreadyReviewed;
-        },
+    hasReview() {
+      return this.existingReview !== null;
     },
-}
+    hasBooking() {
+      return this.booking !== null;
+    },
+    oneColumn() {
+      return !this.loading && this.alreadyReviewed;
+    },
+    twoColumns() {
+      return this.loading || !this.alreadyReviewed;
+    }
+  },
+  methods: {
+    submit() {
+      // 3. Store the review
+      this.errors = null;
+      this.sending = true;
+      axios
+        .post(`/api/reviews`, this.review)
+        .then(response => console.log(response))
+        .catch(err => {
+          if (is422(err)) {
+            const errors = err.response.data.errors;
+            if (errors["content"] && 1 === Object.keys(errors).length) {
+              this.errors = errors;
+              return;
+            }
+          }
+          this.error = true;
+        })
+        .then(() => (this.sending = false));
+    },
+    errorFor(field) {
+      return null !== this.errors && this.errors[field]
+        ? this.errors[field]
+        : null;
+    }
+  }
+};
 </script>
-
-<style>
-
-</style>
---
+++ resources/js/shared/components/FatalError.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-center error">
+      <i class="fas fa-bomb"></i>
+      <h1 class="mt-4">We've encountered a problem</h1>
+      <h2>Unfortunately, we cannot proceed</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .error {
+        color: #999;
+        font-size: 10rem;
+    }
+</style>
