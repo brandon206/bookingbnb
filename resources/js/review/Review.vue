7c6aab83283cb068d68d1de64f62c4160982f041
brandon <brandonpark206@gmail.com>
2020-02-12 22:42:46 -0800

    creating endpoints to handle finding booking by review key, storing reviews in database, updating review ui, adding helper function is404

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,30 +1,52 @@
 <template>
   <div>
-      <div v-if="this.loading">Loading...</div>
-      <div v-else>
-        <div v-if="this.alreadyReviewed">
-            <h3> You've already left a review for this booking</h3>
+      <div class="row" v-if="error">Unknown error has occured, please try again later!</div>
+      <div class="row" v-else>
+        <div :class="[{'col-md-4' : twoColumns}, {'d-none': oneColumn}]">
+            <div class="card">
+                <div class="card-body">
+                    <div v-if="loading">Loading...</div>
+                    <div v-if="hasBooking">
+                        <p>
+                            Stayed at <router-link :to="{name: 'bookable', params: { id: booking.bookable.bookable_id }}">
+                            {{ booking.bookable.title }}
+                            </router-link>
+                        </p>
+                        <p>From {{ booking.from }} to {{ booking.to }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div v-else>
-            <div class="form-group">
-                <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-                <StarRating 
-                    class="fa-3x"
-                    v-model="review.rating"
-                />
-            </div>
-            <div class="form-group">
-                <label for="content" class="text-muted">Describe your experience with</label>
-                <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
-            </div>
+        <div :class="[{'col-md-8' : twoColumns}, {'col-md-12' : oneColumn}]">
+            <div v-if="this.loading">Loading...</div>
+            <div v-else>
+                <div v-if="this.alreadyReviewed">
+                    <h3> You've already left a review for this booking</h3>
+                </div>
+                <div v-else>
+                    <div class="form-group">
+                        <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+                        <StarRating 
+                            class="fa-3x"
+                            v-model="review.rating"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="content" class="text-muted">Describe your experience with</label>
+                        <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+                    </div>
 
-            <button class="btn btn-lg btn-primary btn-block">Submit</button>
+                    <button class="btn btn-lg btn-primary btn-block">Submit</button>
+                </div>
+            </div>
         </div>
       </div>
   </div>
 </template>
 
 <script>
+import { is404 } from "./../shared/utils/response";
+
 export default {
     data() {
         return {
@@ -34,6 +56,8 @@ export default {
             },
             loading: false,
             existingReview: null,
+            booking: null,
+            error: false,
         };
     },
     created() {
@@ -41,17 +65,43 @@ export default {
         //1. if review already exists (in reviews table by id)
         axios
             .get(`/api/reviews/${this.$route.params.id}`)
-            .then(response => (this.existingReview = response.data.data))
+            .then(response => {
+                this.existingReview = response.data.data;
+            })
+            //only goes in here if review doesn't already exist
             .catch(err => {
-            
-            }).then(() => this.loading = false);
-        //2. fetch a booking by a review key
+                if(is404(err)) {
+                    //2. fetch a booking by a review key
+                    return axios.get(`/api/booking-by-review/${this.$route.params.id}`).then(response => {
+                        this.booking = response.data.data;
+                    }).catch((err) => {
+                        if(! is404(err)){
+                            this.error = true;
+                        }
+                    });
+                }
+                this.error = true;
+            }).then(() => {
+                this.loading = false;
+            });
         //3. store the review
     },
     computed: {
         alreadyReviewed() {
+            return this.hasReview || ! this.booking;
+        },
+        hasReview() {
             return this.existingReview !== null;
-        }
+        },
+        hasBooking() {
+            return this.booking !== null;
+        },
+        oneColumn() {
+            return ! this.loading && this.alreadyReviewed;
+        },
+        twoColumns() {
+            return this.loading || ! this.alreadyReviewed;
+        },
     },
 }
 </script>
