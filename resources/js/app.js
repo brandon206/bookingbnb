62bf8a5bb40d4cd258b7c012110704e5cbe9349b
brandon <brandonpark206@gmail.com>
2020-03-01 14:16:27 -0800

    refactoring axios calls to use async await & try/catch, implementing vue slots

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import moment from 'moment';
 
 import StarRating from "./shared/components/StarRating";
 import FatalError from "./shared/components/FatalError";
+import Success from "./shared/components/Success";
 import ValidationErrors from "./shared/components/ValidationErrors";
 
 window.Vue = require('vue');
@@ -17,6 +18,7 @@ Vue.filter("fromNow", value => moment(value).fromNow());
 
 Vue.component("StarRating", StarRating);
 Vue.component("FatalError", FatalError);
+Vue.component("success", Success);
 Vue.component("v-errors", ValidationErrors);
 
 const app = new Vue({
